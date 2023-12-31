109d64095b0a1980671e387d3abf6c93d359ecc8
Per Linde <per@itablera.se>
2023-12-31 16:59:39 +0100

    Delete failed plugin attempt

--- src/mdx-editor-plugins/core/SharedHistoryPlugin.tsx
+++
@@ -1,8 +0,0 @@
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin.js'
-import React from 'react'
-import { corePluginHooks } from '.'
-
-export const SharedHistoryPlugin = () => {
-  const [historyState] = corePluginHooks.useEmitterValues('historyState')
-  return <HistoryPlugin externalHistoryState={historyState} />
-}
