456d7091fec3e74eb4d411e934f6e3c72e920b34
Per Linde <per@itablera.se>
2024-01-06 12:52:47 +0100

    Handle mdxExpression in catch all

--- src/mdx-editor-plugins/catch-all/CatchAllVisitor.ts
+++ src/mdx-editor-plugins/catch-all/CatchAllVisitor.ts
@@ -1,12 +1,18 @@
 import { MdastImportVisitor } from "@mdxeditor/editor";
-import { $createTextNode } from 'lexical'
+import { $createTextNode, $createParagraphNode } from 'lexical'
 import Mdast from 'mdast';
 
 export const CatchAllVisitor: MdastImportVisitor<Mdast.Content> = {
     testNode: () => true,
     visitNode: ({ mdastNode, actions }) => {
-        console.warn('catch all', { mdastNode });
-        actions.addAndStepInto($createTextNode());
+        const paragraph = $createParagraphNode()
+    
+        if (mdastNode.type === 'mdxFlowExpression' || mdastNode.type === 'mdxTextExpression') {
+          paragraph.append($createTextNode(mdastNode.value))
+        }
+    
+        console.warn('catch all', { mdastNode })
+        actions.addAndStepInto(paragraph)
     },
     priority: -500
 };
