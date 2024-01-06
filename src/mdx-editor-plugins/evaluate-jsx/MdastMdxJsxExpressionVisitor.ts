c8e18e5836ba6f7673929146187413ed22b48871
Per Linde <per@itablera.se>
2024-01-06 13:10:53 +0100

    Fix missing parent node by adding paragraph

--- src/mdx-editor-plugins/evaluate-jsx/MdastMdxJsxExpressionVisitor.ts
+++ src/mdx-editor-plugins/evaluate-jsx/MdastMdxJsxExpressionVisitor.ts
@@ -1,5 +1,5 @@
 import { MdastImportVisitor } from '@mdxeditor/editor'
-import { $createTextNode } from 'lexical'
+import { $createTextNode, $createParagraphNode } from 'lexical'
 import { MdxFlowExpression } from 'mdast-util-mdx'
 
 export const MdastMdxJsxExpressionVisitor: MdastImportVisitor<MdxFlowExpression> = {
@@ -7,10 +7,9 @@ export const MdastMdxJsxExpressionVisitor: MdastImportVisitor<MdxFlowExpression>
     return node.type === 'mdxTextExpression' || node.type === 'mdxFlowExpression'
   },
   visitNode({ mdastNode, actions }) {
-    /**
-     * Assumes there is a parent ElementNode or it will fail as only ElementNodes or Decorator nodes may be appended to Root node
-     */
-    actions.addAndStepInto($createTextNode(eval(mdastNode.value) as string).setFormat(actions.getParentFormatting()))
+    const paragraph = $createParagraphNode()
+    paragraph.append($createTextNode(eval(mdastNode.value) as string).setFormat(actions.getParentFormatting()))
+    actions.addAndStepInto(paragraph)
   },
   priority: -200
 }
